a0c5f8a3336a0ded8b07826540ba44293296d806
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-03-27 18:29:23 -0500

    commit tree c0077e868563bc3e07bef0f18b8b88b1cdd4aad3

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+function findMatching(driverArray, string){
+    const filteredArray = driverArray.filter(driver => {
+        return driver.toUpperCase() === string.toUpperCase()
+    })
+
+    return filteredArray
+}
+
+function fuzzyMatch(driverArray, string){
+    const filteredArray = driverArray.filter( driver => {
+        return driver.slice(0, [string.length]).toUpperCase() === string.toUpperCase()
+    })
+
+    return filteredArray
+}
+
+function matchName(driverArray, string){
+    const filteredArray = driverArray.filter( driver => {
+        return driver.name.toUpperCase() === string.toUpperCase()
+    })
+
+    return filteredArray
+}
